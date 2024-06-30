1382448ddd559b1c74d41be6ef22409f360f98b1
ibnaleem <ibnaleem@outlook.com>
2024-06-30 20:48:36 +0100

    handle AbortDownloadException: Instagram redirects logins after a specified time, this commit should handle the issue.
    Mandate all users to manually login to their Instagram accounts via `instaloader.Instaloader().login(user="USERNAME", passwd="PASSWORD")`

--- main.py
+++ main.py
@@ -19,35 +19,42 @@ ascii_art = """
 class InstaTracker:
     def __init__(self, username: str):
         self.username: str = username
-        self.bot = instaloader.Instaloader()
+        self.instaloader = instaloader
+        self.bot = self.instaloader.Instaloader()
         self.console = Console()
         self.date = datetime.datetime.now(datetime.UTC)
 
     def get_followers(self):
-        profile = instaloader.Profile.from_username(self.bot.context, self.username)
+        profile = self.instaloader.Profile.from_username(self.bot.context, self.username)
         return profile.followers
 
     def get_following(self):
-        profile = instaloader.Profile.from_username(self.bot.context, self.username)
+        profile = self.instaloader.Profile.from_username(self.bot.context, self.username)
         return profile.followees
 
     def get_posts(self):
-        profile = instaloader.Profile.from_username(self.bot.context, self.username)
+        profile = self.instaloader.Profile.from_username(self.bot.context, self.username)
         return profile.mediacount
 
     def get_bio(self):
-        profile = instaloader.Profile.from_username(self.bot.context, self.username)
+        profile = self.instaloader.Profile.from_username(self.bot.context, self.username)
         return profile.biography
 
     def write_to_file(self, data):
         with open(f"{self.username}_logs.txt", "a") as f:
             f.write(f"{data}\n")
-    def main(self, args=None):
-        if not args:
-            os.system("clear" if not os.name == "nt" else "cls")
-            self.console.print(ascii_art, style="bold blue")
-            bot_username = input("Enter your Instagram username: ")
-            self.bot.interactive_login(bot_username)
+    def main(self):
+        os.system("clear" if not os.name == "nt" else "cls")
+        self.console.print(ascii_art, style="bold blue")
+        print("Logging in...")
+
+        # You must manually login to avoid any login errors. Below, replace the fields 'user'
+        # and 'passwd' with your username and password. If you'd like, you can create a brand
+        # new Instagram only for this script. Usernames and passwords are never logged. 
+        # If this script is throwing BadCredentials exception, use a different Instagram account.
+       
+        self.bot.login(user="YOUR INSTAGRAM USERNAME", passwd="YOUR INSTAGRAM PASSWORD") # this allows us to access & scrape Instagram.
+     
         self.console.print(f"[bold green]Welcome to InstaTracker[/bold green]")
         self.console.print(f"[bold blue]Getting initial data for {self.username}...[/bold blue]")
         initial = {"followers": self.get_followers(), "following": self.get_following(), "posts": self.get_posts(), "bio": self.get_bio()}
@@ -60,52 +67,44 @@ class InstaTracker:
         self.console.print(f"[bold green]Tracking started! I will log all changes to {self.username}_logs.txt and this terminal![/bold green]")
 
         while True:
-            try:
-                time.sleep(300)
-                if self.get_followers() != initial["followers"]:
-                    if initial["followers"] > self.get_followers():
-                        self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                        self.console.print(f"[bold red]{self.username} has lost {initial['followers'] - self.get_followers()} followers ({initial['followers']} followers --> {self.get_followers()} followers)[/bold red]")
-                        self.write_to_file(f"------{self.date}------\n{self.username} has lost {initial['followers'] - self.get_followers()} followers ({initial['followers']} followers --> {self.get_followers()} followers)")
-                    else:
-                        self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                        self.console.print(f"[bold green]{self.username} has gained {self.get_followers() - initial['followers']} followers ({initial['followers']} followers --> {self.get_followers()} followers)[/bold green]")
-                        self.write_to_file(f"------{self.date}------\n{self.username} has gained {self.get_followers() - initial['followers']} followers ({initial['followers']} followers --> {self.get_followers()} followers)")
-                    initial["followers"] = self.get_followers()
-                if self.get_following() != initial["following"]:
-                    if initial["following"] > self.get_following():
-                        self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                        self.console.print(f"[bold red]{self.username} has unfollowed {initial['following'] - self.get_following()} people ({initial['following']} following --> {self.get_following()} following)[/bold red]")
-                        self.write_to_file(f"------{self.date}------\n{self.username} has unfollowed {initial['following'] - self.get_following()} people ({initial['following']} following --> {self.get_following()} following)")
-                    else:
-                        self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                        self.console.print(f"[bold green]{self.username} has followed {self.get_following() - initial['following']} people ({initial['following']} following --> {self.get_following()} following)[/bold green]")
-                        self.write_to_file(f"------{self.date}------\n{self.username} has followed {self.get_following() - initial['following']} people ({initial['following']} following --> {self.get_following()} following)")
-                    initial["following"] = self.get_following()
-                if self.get_posts() != initial["posts"]:
-                    if initial["posts"] > self.get_posts():
-                        self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                        self.console.print(f"[bold red]{self.username} has deleted {initial['posts'] - self.get_posts()} posts ({initial['posts']} posts --> {self.get_posts()} posts)[/bold red]")
-                        self.write_to_file(f"------{self.date}------\n{self.username} has deleted {initial['posts'] - self.get_posts()} posts ({initial['posts']} posts --> {self.get_posts()} posts)")
-                    else:
-                        self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                        self.console.print(f"[bold green]{self.username} has posted {self.get_posts() - initial['posts']} posts ({initial['posts']} posts --> {self.get_posts()} posts)[/bold green]")
-                        self.write_to_file(f"------{self.date}------\n{self.username} has posted {self.get_posts() - initial['posts']} posts ({initial['posts']} posts --> {self.get_posts()} posts)")
-                    initial["posts"] = self.get_posts()
-                if self.get_bio() != initial["bio"]:
+            time.sleep(300)
+            if self.get_followers() != initial["followers"]:
+                if initial["followers"] > self.get_followers():
                     self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                    self.console.print(f"[bold green]{self.username} has updated their bio:[/bold green]")
-                    self.console.print(f"[bold red] Before: {initial['bio']}[/bold red]")
-                    self.console.print(f"[bold green] After: {self.get_bio()}[/bold green]")
-                    self.write_to_file(f"------{self.date}------\n{self.username} has updated their bio:\nBefore: {initial['bio']}\nAfter: {self.get_bio()}")
-                    initial["bio"] = self.get_bio()
-            except AbortDownloadException:
+                    self.console.print(f"[bold red]{self.username} has lost {initial['followers'] - self.get_followers()} followers ({initial['followers']} followers --> {self.get_followers()} followers)[/bold red]")
+                    self.write_to_file(f"------{self.date}------\n{self.username} has lost {initial['followers'] - self.get_followers()} followers ({initial['followers']} followers --> {self.get_followers()} followers)")
+                else:
+                    self.console.print(f"[bold red]------{self.date}------[/bold red]")
+                    self.console.print(f"[bold green]{self.username} has gained {self.get_followers() - initial['followers']} followers ({initial['followers']} followers --> {self.get_followers()} followers)[/bold green]")
+                    self.write_to_file(f"------{self.date}------\n{self.username} has gained {self.get_followers() - initial['followers']} followers ({initial['followers']} followers --> {self.get_followers()} followers)")
+                initial["followers"] = self.get_followers()
+            if self.get_following() != initial["following"]:
+                if initial["following"] > self.get_following():
+                    self.console.print(f"[bold red]------{self.date}------[/bold red]")
+                    self.console.print(f"[bold red]{self.username} has unfollowed {initial['following'] - self.get_following()} people ({initial['following']} following --> {self.get_following()} following)[/bold red]")
+                    self.write_to_file(f"------{self.date}------\n{self.username} has unfollowed {initial['following'] - self.get_following()} people ({initial['following']} following --> {self.get_following()} following)")
+                else:
+                    self.console.print(f"[bold red]------{self.date}------[/bold red]")
+                    self.console.print(f"[bold green]{self.username} has followed {self.get_following() - initial['following']} people ({initial['following']} following --> {self.get_following()} following)[/bold green]")
+                    self.write_to_file(f"------{self.date}------\n{self.username} has followed {self.get_following() - initial['following']} people ({initial['following']} following --> {self.get_following()} following)")
+                initial["following"] = self.get_following()
+            if self.get_posts() != initial["posts"]:
+                if initial["posts"] > self.get_posts():
+                    self.console.print(f"[bold red]------{self.date}------[/bold red]")
+                    self.console.print(f"[bold red]{self.username} has deleted {initial['posts'] - self.get_posts()} posts ({initial['posts']} posts --> {self.get_posts()} posts)[/bold red]")
+                    self.write_to_file(f"------{self.date}------\n{self.username} has deleted {initial['posts'] - self.get_posts()} posts ({initial['posts']} posts --> {self.get_posts()} posts)")
+                else:
+                    self.console.print(f"[bold red]------{self.date}------[/bold red]")
+                    self.console.print(f"[bold green]{self.username} has posted {self.get_posts() - initial['posts']} posts ({initial['posts']} posts --> {self.get_posts()} posts)[/bold green]")
+                    self.write_to_file(f"------{self.date}------\n{self.username} has posted {self.get_posts() - initial['posts']} posts ({initial['posts']} posts --> {self.get_posts()} posts)")
+                initial["posts"] = self.get_posts()
+            if self.get_bio() != initial["bio"]:
                 self.console.print(f"[bold red]------{self.date}------[/bold red]")
-                self.console.print(f"[bold red]You've been logged out, tracking has been paused[/bold red]")
-                self.write_to_file(f"------{self.date}------\nYou've been logged out, tracking has been paused")
-                self.console.print("[bold red]Please login again...[/bold red]")
-                self.bot.interactive_login(bot_username)
-                self.main("args")
+                self.console.print(f"[bold green]{self.username} has updated their bio:[/bold green]")
+                self.console.print(f"[bold red] Before: {initial['bio']}[/bold red]")
+                self.console.print(f"[bold green] After: {self.get_bio()}[/bold green]")
+                self.write_to_file(f"------{self.date}------\n{self.username} has updated their bio:\nBefore: {initial['bio']}\nAfter: {self.get_bio()}")
+                initial["bio"] = self.get_bio()
 if __name__ == "__main__":
     parser = ArgumentParser(prog="Instagram Tracker",
         description="📸 an Instagram tracker that logs any changes to an Instagram account (followers, following, posts, and bio)",
@@ -113,4 +112,9 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--username", help="The username of the account to track", required=True)
     args = parser.parse_args()
     tracker = InstaTracker(str(args.username))
-    tracker.main()
+    try:
+        tracker.main()
+    except Exception:
+        tracker.console.print(f"[bold red]------{tracker.date}------\nLogged out of Instagram... Logging in again....[/bold red]")
+        tracker.write_to_file(f"------{tracker.date}------\nLogged out of Instagram... Logging in again....")
+        tracker.main()
